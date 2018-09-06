348941438863a09da387e7dd138e62d96ded0115
eryshkov <wendor265>
2018-09-06 21:14:17 +0300

    initial commit

---
+++ Personality Quiz 2/QuestionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  QuestionViewController.swift
+//  Personality Quiz 2
+//
+//  Created by Evgeniy Ryshkov on 06.09.2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
