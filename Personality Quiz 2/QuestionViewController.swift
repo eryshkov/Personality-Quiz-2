db74650f68292b4a5172cca65bf2f08c0e84eb53
eryshkov <wendor265>
2018-09-10 20:54:40 +0300

    added questions

--- Personality Quiz 2/QuestionViewController.swift
+++ Personality Quiz 2/QuestionViewController.swift
@@ -9,6 +9,35 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
+    
+    var questions: [Question] = [
+        Question(text: "Какая еда вам нравится?",
+                 type: .single,
+                 answers: [
+                    Answer(text: "Стейк", type: .dog),
+                    Answer(text: "Рыба", type: .cat),
+                    Answer(text: "Морковка", type: .rabbit),
+                    Answer(text: "Кукуруза", type: .turtle),
+            ]),
+        Question(text: "Что вам нравится делать?",
+                 type: .multiple,
+                 answers: [
+                    Answer(text: "Есть", type: .dog),
+                    Answer(text: "Спать", type: .cat),
+                    Answer(text: "Прыгать", type: .rabbit),
+                    Answer(text: "Плавать", type: .turtle),
+            ]),
+        Question(text: "Насколько вам нравятся поездки на машине?",
+                 type: .ranged,
+                 answers: [
+                    Answer(text: "Ненавижу", type: .cat),
+                    Answer(text: "Немного нервничаю", type: .rabbit),
+                    Answer(text: "Почти не замечаю", type: .turtle),
+                    Answer(text: "Обожаю", type: .dog),
+            ]),
+    ]
+    
+    var questionIndex = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,20 +45,8 @@ class QuestionViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updateUI() {
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
