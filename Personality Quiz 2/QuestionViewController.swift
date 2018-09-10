b722e033ce5b6b857aa56d8c7c3fec5c3bd18290
eryshkov <wendor265>
2018-09-10 21:09:02 +0300

    implement updateUI()

--- Personality Quiz 2/QuestionViewController.swift
+++ Personality Quiz 2/QuestionViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class QuestionViewController: UIViewController {
     
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet weak var rangedStackView: UIStackView!
+    
     var questions: [Question] = [
         Question(text: "Какая еда вам нравится?",
                  type: .single,
@@ -42,11 +46,26 @@ class QuestionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
     func updateUI() {
+        singleStackView.isHidden = true
+        multipleStackView.isHidden = true
+        rangedStackView.isHidden = true
         
+        navigationItem.title = "Ворос \(questionIndex + 1)"
+        
+        let currentQuestion = questions[questionIndex]
+        
+        switch currentQuestion.type {
+        case .single:
+            singleStackView.isHidden = false
+        case .multiple:
+            multipleStackView.isHidden = false
+        case .ranged:
+            rangedStackView.isHidden = false
+        }
     }
 
 }
